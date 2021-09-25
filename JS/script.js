c08cab963a2cc61330d7cff5df2f3b542eb25a8a
felipepadua011 <89053801+felipepadua011@users.noreply.github.com>
2021-09-24 22:54:48 -0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,30 @@
+
+const botao = document.querySelector("#botao1"); 
+const nome = document.querySelector("#nomed");
+const humor = document.querySelector("#statusd");
+const imagem = document.querySelector("#imagem1");
+
+window.onload = function() {
+
+};
+
+botao.addEventListener("click", function () { 
+
+    if (nome.innerText == "Nome: Robert Downey Jr") {
+
+        nome.innerText = "Nome: Robert";
+
+        humor.innerText = "Status: Sério 😠";
+
+        imagem.src="media/imagem/Sério.jpg";
+
+    } else if (nome.innerText == "Nome: Robert") {
+
+        nome.innerText = "Nome: Iron man";
+
+        humor.innerText = "Status: Puto 🤬";
+
+        imagem.src="media/imagem/bravo.jpg";
+    }
+});
+
